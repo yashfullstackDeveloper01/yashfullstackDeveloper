ececfd13b90b9706288c444829ddae89f28ed1b9
yashfullstackDeveloper01 <yashdhapke2@gmail.com>
2025-04-01 20:59:52 +0530

    Add files via upload

---
+++ YASHDHAPKE/class.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  if (req.url === "/students") {
+    res.write("<html");
+    res.write("<head><title>Students</title></head");
+    res.write("<body><h1>Student</h1>");
+    res.write("<ul><li>alekh</li><li>ayush</li><li>atharv</li></ul>");
+    res.write("</body></html>");
+
+    return res;
+  }
+  if (req.url == "/") {
+    res.write(
+      `<input style=" height:6.5%; " type="text"placeholder="username"/><br><input style=" height:6.5%; " type="username" placeholder="password"/><br><button style=" height:6.5%; background-color: lightblue; margin-left:35px;" >submit</button> `
+    );
+    res.end();
+  }
+});
+const address = "127.0.0.1";
+const port = 50034;
+server.listen(port, address, () => { 
+  console.log("server is running on http://" + address + ":" + port);
+});
+        
